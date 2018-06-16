df263b958990d3838f7f6b400d597523fc42a026
Marco Guastalli <marco.guastalli@netcentric.biz>
2018-06-16 18:53:12 +0200

    first commit

---
+++ src/main/java/net/marco27/apps/javagradlehelloworld/JavaGradleHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package net.marco27.apps.javagradlehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGradleHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGradleHelloWorldApplication.class, args);
+	}
+}
---
+++ src/test/java/net/marco27/apps/javagradlehelloworld/JavaGradleHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package net.marco27.apps.javagradlehelloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaGradleHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
